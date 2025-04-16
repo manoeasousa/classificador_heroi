6e4d7e60e98bdb99298509ff92891b4102dbb859
Emanuel Sousa <mano.sousa@gmail.com>
2025-04-16 10:12:21 -0300

    commit 9

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ switch(XP){
     case "XP < 1.000":
     console.log ("O herói " + nomeHeroi + "está no nível Ferro")
 break
-    case "XP > 1001 && < 2.000":
+    case "XP > 1001 && XP < 2.000":
     console.log ("O herói " + nomeHeroi + "está no nível Bronze")
 break
-    case "XP > 2.001 && < 5.000":
+    case "XP > 2.001 && XP < 5.000":
     console.log ("O herói " + nomeHeroi + "está no nível Prata")
 break
-    case "XP > 5.001 && < 7.000":
+    case "XP > 5.001 && XP < 7.000":
     console.log ("O herói " + nomeHeroi + "está no nível Ouro")
 break
-    case "XP > 7.001 && < 8.000":
+    case "XP > 7.001 && XP < 8.000":
     console.log ("O herói " + nomeHeroi + "está no nível Platina")
 break
-    case "XP >= 8.001 && <= 9.000":
+    case "XP >= 8.001 && XP <= 9.000":
     console.log ("O herói " + nomeHeroi + "está no nível Ascendente")
 break
-    case "XP > 9.001 && < 10.000":
+    case "XP > 9.001 && XP < 10.000":
     console.log ("O herói " + nomeHeroi + "está no nível Imortal")
 break
     case "XP >= 10001":
